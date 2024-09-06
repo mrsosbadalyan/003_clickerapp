8b561e0379cd28a7e9f15a7a198e19c4d7fe52a0
Sos Badalyan <mrsosbadalyan@gmail.com>
2024-09-06 18:42:32 +0400

    Creating first button and counter

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
- <h2> Hello, World! </h2>
+  <div>
+  <h2>{{count}}</h2>
+  <button @click="increment">1000 clicks are you will calm down</button>
+  </div>
 </template>
 
 <script>
-export default {
+import {ref} from 'vue'; 
 
+export default {
+  setup () {
+    const count = ref(0);
+
+    function increment() {
+      count.value++;
+    }
+
+    return {
+      count,
+      increment
+    }
+  }
 }
 </script>
 
